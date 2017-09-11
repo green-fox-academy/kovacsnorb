36ea9ee65bcd858a1fb140565dfc1dc4143c507d
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 16:17:29 +0200

    Week2/Day1 - 16-AnimalsAndLegs is done

---
+++ week-02/day-01/16-AnimalsAndLegs/16-AnimalsAndLegs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _16_AnimalsAndLegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+            Console.Write("How many chickens has the farmer? ");
+            int numberOfChickens = int.Parse(Console.ReadLine());
+            Console.Write("How many pigs has the farmer? ");
+            int numberOfPigs = int.Parse(Console.ReadLine());
+            int numberOfLegs = numberOfChickens * 2 + numberOfPigs * 4;
+            Console.WriteLine("The total number of legs are: " + numberOfLegs);
+            Console.ReadLine();
+        }
+    }
+}
