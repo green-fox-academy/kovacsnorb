432123baf8032035c7088ebe6e7b5e82baad417a
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 14:13:13 +0200

    Week2/Day1 - 08_Swap is done

---
+++ week-02/day-01/08-Swap/08-Swap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08_Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Swap the values of the wariables
+            int a = 123;
+            int b = 526;
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("Value of 'a' is: " + a + ", value of 'b' is: " + b);
+            Console.ReadLine();
+        }
+    }
+}
