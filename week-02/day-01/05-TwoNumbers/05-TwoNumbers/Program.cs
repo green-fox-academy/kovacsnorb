628121ced9931e809c313e6145e3453399cfb012
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 13:36:47 +0200

    Week2/Day1 - 05-TwoNumbers is done

---
+++ week-02/day-01/05-TwoNumbers/05-TwoNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05_TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints a few operations on two numbers: 22 and 13
+
+            // Print the result of 13 added to 22
+
+            // Print the result of 13 substracted from 22
+
+            // Print the result of 22 mutiplied by 13
+
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+
+            // Print the integer part of 22 divided by 13
+
+            // Print the reminder of 22 divided by 13
+            Console.WriteLine("13 + 22 = " + (13 + 22));
+            Console.WriteLine("22 - 13 = " + (22 - 13));
+            Console.WriteLine("22 x 13 = " + (22 * 13));
+            Console.WriteLine("22.0 / 13.0 = " + (22.0 / 13.0));
+            Console.WriteLine("22 / 13 = " + (22 / 13));
+            Console.WriteLine("22 % 13 = " + (22 % 13));
+            Console.ReadLine();
+        }
+    }
+}
