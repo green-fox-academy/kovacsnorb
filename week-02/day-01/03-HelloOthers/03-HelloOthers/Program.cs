d8194480ae2f71d6dd79f01259b0525acb98c67b
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 13:06:10 +0200

    Week2/Day1 - 03-HelloOthers is done

---
+++ week-02/day-01/03-HelloOthers/03-HelloOthers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03_HelloOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Greet 3 of your classmates with this program, in three separate lines
+            // like:
+            //
+            // Hello, Esther!
+            // Hello, Mary!
+            // Hello, Joe!
+            Console.WriteLine("Hello, Adri!");
+            Console.WriteLine("Hello, Dorka!");
+            Console.WriteLine("Hello, Reka!");
+            Console.ReadLine();
+        }
+    }
+}
