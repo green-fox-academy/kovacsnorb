20c04dba1997e3f83a7593c0937e00a0998ee7ad
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 16:01:35 +0200

    Week2/Day1 - 15-MileToKmConverter is done

---
+++ week-02/day-01/15-MileToKmConverter/15-MileToKmConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _15_MileToKmConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+
+            Console.Write("Write a distance in kilometers: ");
+            int kilometer = int.Parse(Console.ReadLine());
+
+            double miles = kilometer * 0.621371192;
+            Console.WriteLine(kilometer + " kilometer(s) is/are " + miles + " miles.");
+            Console.ReadLine();
+        }
+    }
+}
