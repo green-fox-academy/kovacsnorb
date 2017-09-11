b7f2ac88cc1e42a78c45d91a99660e9c90f5ccf8
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 15:50:09 +0200

    Week2/Day1 - 14-HelloUser is done

---
+++ week-02/day-01/14-HelloUser/14-HelloUser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _14_HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+
+            Console.Write("What's your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.ReadLine();
+
+        }
+    }
+}
--- week-02/day-01/test/test/Program.cs
+++ week-02/day-01/test/test/Program.cs
@@ -6,56 +6,17 @@ namespace test
     {
         static void Main(string[] args)
         {
-            int a = 12;
-            a += 4;
-            // prints 16
-            Console.WriteLine(a);
+            Console.WriteLine("Hello user what is your name?");
+            string name = Console.ReadLine();
 
-            int b = 12;
-            b -= 4;
-            // prints 8
-            Console.WriteLine(b);
+            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("How old are you? ");
 
-            int c = 12;
-            // prints 12
-            Console.WriteLine(c++);
-            // prints 13
-            Console.WriteLine(c);
-
-            int d = 12;
-            // prints 13
-            Console.WriteLine(++d);
-            // prints 13
-            Console.WriteLine(d);
-
-            int e = 12;
-            // prints 12
-            Console.WriteLine(e--);
-            // prints 11
-            Console.WriteLine(e);
-
-            int f = 12;
-            // prints 11
-            Console.WriteLine(--f);
-            // prints 11
-            Console.WriteLine(f);
-
-            int g = 12;
-            g *= 3;
-            // prints 36
-            Console.WriteLine(g);
-
-            int h = 12;
-            h /= 3;
-            // prints 4
-            Console.WriteLine(h);
-
-            int i = 12;
-            i %= 7;
-            // prints 5
-            Console.WriteLine(i);
+            string age = Console.ReadLine();
+            Console.WriteLine(age + " is a nice age");
 
             Console.ReadLine();
+
         }
     }
 }
