0839ce1111dcaf326f91ff8b1b6ee19a71a82c89
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 14:05:16 +0200

    Week2/Day1 - 07_FavouriteNumber is done

---
+++ week-02/day-01/07-FavouriteNumber/07-FavouriteNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07_FavouriteNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Store your favorite number in a variable (as a number)
+            // And print it like this: "My favorite number is: 8"
+            int favouriteNumber = 16;
+            Console.WriteLine("My favourite number is: " + favouriteNumber);
+            Console.ReadLine();
+        }
+    }
+}
--- week-02/day-01/test/test/Program.cs
+++ week-02/day-01/test/test/Program.cs
@@ -6,17 +6,54 @@ namespace test
     {
         static void Main(string[] args)
         {
-            // string and special characters
-            Console.WriteLine("hello");
-            Console.WriteLine("don't");
-            Console.WriteLine("She said, \"you deserva a treat\" ");
+            int a = 12;
+            a += 4;
+            // prints 16
+            Console.WriteLine(a);
 
-            // string Operators
-            // Concatination
-            Console.WriteLine("hand" + "some");
+            int b = 12;
+            b -= 4;
+            // prints 8
+            Console.WriteLine(b);
 
-            // concat string with number
-            Console.WriteLine("My favourite number is: " + 8);
+            int c = 12;
+            // prints 12
+            Console.WriteLine(c++);
+            // prints 13
+            Console.WriteLine(c);
+
+            int d = 12;
+            // prints 13
+            Console.WriteLine(++d);
+            // prints 13
+            Console.WriteLine(d);
+
+            int e = 12;
+            // prints 12
+            Console.WriteLine(e--);
+            // prints 11
+            Console.WriteLine(e);
+
+            int f = 12;
+            // prints 11
+            Console.WriteLine(--f);
+            // prints 11
+            Console.WriteLine(f);
+
+            int g = 12;
+            g *= 3;
+            // prints 36
+            Console.WriteLine(g);
+
+            int h = 12;
+            h /= 3;
+            // prints 4
+            Console.WriteLine(h);
+
+            int i = 12;
+            i %= 7;
+            // prints 5
+            Console.WriteLine(i);
 
             Console.ReadLine();
         }
