f0449f02db61451304809f55e4cc125d77e81cfc
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 15:28:29 +0200

    Week2/Day1 - 12-Cuboid is done

---
+++ week-02/day-01/12-Cuboid/12-Cuboid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _12_Cuboid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            double a = 10;
+            double b = 20;
+            double c = 50;
+
+            Console.WriteLine("a = " + a + ", b = " + b + ", c = " + c);
+
+            double surface = 2 * (a * b + a * c + b * c);
+            double volume = a * b * c;
+
+            Console.WriteLine("Surface: " + surface + ", volume: " + volume);
+            Console.ReadLine();
+        }
+    }
+}
