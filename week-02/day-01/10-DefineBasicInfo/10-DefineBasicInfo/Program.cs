9b61e88661e783f7f4b6e54b6dd33f9050aede21
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 14:28:53 +0200

    Week2/Day1 - 10_DefineBasicInfo is done

---
+++ week-02/day-01/10-DefineBasicInfo/10-DefineBasicInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10_DefineBasicInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define several things as a variable than print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Wether you are married or not as a boolean
+            string name = "Norbi";
+            int age = 31;
+            double height = 1.68;
+            bool isMarried = false;
+            Console.WriteLine("My name is: " + name + ", my age is: " + age + ", my height is: " + height + ", my marriage status is: " + isMarried);
+            Console.ReadLine();
+        }
+    }
+}
