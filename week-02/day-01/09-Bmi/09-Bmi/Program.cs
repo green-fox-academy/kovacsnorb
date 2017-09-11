1ccc827ae6fdbd63b762ebcd0aa1f752005f65cd
kovacsnorb <kovacsnorb@gmail.com>
2017-09-11 14:18:19 +0200

    Week2/Day1 - 09_Bmi is done

---
+++ week-02/day-01/09-Bmi/09-Bmi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _09_Bmi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+            Console.WriteLine("BMI is: " + (massInKg / (heightInM * heightInM)));
+            Console.ReadLine();
+        }
+    }
+}
