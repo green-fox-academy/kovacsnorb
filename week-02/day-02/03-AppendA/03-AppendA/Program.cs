fd9eb14324c02ab8a256b345beecb31e48d710cc
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 13:30:08 +0200

    Week2/Day2 - 03-AppendA is done

---
+++ week-02/day-02/03-AppendA/03-AppendA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03_AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input
+            //   and appends an 'a' character to its end
+            // - Print the result of `appendA(am)`
+            string am = "kuty";
+            Console.WriteLine(AppendA(am));
+            Console.ReadLine();
+        }
+
+        public static string AppendA(string input)
+        {
+            return input + "a";
+        }
+    }
+}
