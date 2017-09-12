5db1dc8ee18ff2372b5c2b55fe896e2efe04a4ee
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 17:26:46 +0200

    Week2/Day2 - 11-IncrementElement is done

---
+++ week-02/day-02/11-IncrementElement/11-IncrementElement/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _11_IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+            int[] t = new int[5] { 1, 2, 3, 4, 5 };
+            t[2]++;
+            Console.WriteLine(t[2]);
+            Console.ReadLine();
+        }
+    }
+}
