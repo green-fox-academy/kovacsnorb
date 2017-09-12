6dc6f8a12e9ea2e4a0b9b8a3dc6885f7e6f7165f
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 13:39:22 +0200

    Week2/Day2 - 04-Summing is done

---
+++ week-02/day-02/04-Summing/04-Summing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04_Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            Console.Write("Give me a parameter: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Sum(number));
+            Console.ReadLine();
+        }
+
+        public static int Sum(int parameterToSum)
+        {
+            int sum = 0;
+            for (int i = parameterToSum; i > 0; i--)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
