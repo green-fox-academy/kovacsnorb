5d2a1f3c031cf3726eb3e2eaa504f91067a20b38
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 23:05:46 +0200

    Week2/Day2 - 14-DoubleItems is done

---
+++ week-02/day-02/14-DoubleItems/14-DoubleItems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _14_DoubleItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ag`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Double all the values in the array
+            int[] ag = new int[5] { 3, 4, 5, 6, 7 };
+            for (int i = 0; i < ag.Length; i++)
+            {
+                ag[i] = 2 * ag[i];
+            }
+
+            for (int i = 0; i < ag.Length; i++)
+            {
+                Console.Write("{0}, ", ag[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
