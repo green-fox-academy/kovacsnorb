5d063001563537c5be25023b623151691942f0f2
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 23:39:47 +0200

    Week2/Day2 - 16-AppendA is done

---
+++ week-02/day-02/16-AppendA/16-AppendA/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _16_AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            string[] nimals = new string[3] { "kuty", "macsk", "cic" };
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                nimals[i] = nimals[i] + "a";
+            }
+
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                Console.Write("{0}, " , nimals[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
