724e4b6d4210075fbff79625a0033e8128884dc4
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 13:11:45 +0200

    Week2/Day2 - 01-Doubling is done

---
+++ week-02/day-02/01-Doubling/01-Doubling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01_Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `ak` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter
+            // - Print the result of `doubling(ak)`
+
+            int ak = 123;
+            Console.WriteLine(Doubling(ak));
+            Console.ReadLine();
+        }
+
+        public static int Doubling(int number01)
+        {
+            return 2 * number01;
+        }
+    }
+}
