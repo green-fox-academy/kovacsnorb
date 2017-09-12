3868bab756b396db2b9baa7c711ee9878d699357
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 23:50:55 +0200

    Week2/Day2 - 17-SwapElements is done

---
+++ week-02/day-02/17-SwapElements/17-SwapElements/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _17_SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+            string[] abc = new string[3] { "first", "second", "third" };
+            string temp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = temp;
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Console.Write("{0}, ", abc[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
