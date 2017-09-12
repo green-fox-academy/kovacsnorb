04f8cfe46b4434c10b29b8e131a832de46bd6658
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 17:19:54 +0200

    Week2/Day2 - 10-ChangeElements is done

---
+++ week-02/day-02/10-ChangeElement/10-ChangeElement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _10_ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `s`
+            //   with the following content: `[1, 2, 3, 8, 5, 6]`
+            // - Change the 8 to 4
+            // - Print the fourth element
+            int[] s = new int[6] { 1, 2, 3, 8, 5, 6 };
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 8)
+                    s[i] = 4;
+            }
+            Console.WriteLine(s[3]);
+            Console.ReadLine();
+        }
+    }
+}
