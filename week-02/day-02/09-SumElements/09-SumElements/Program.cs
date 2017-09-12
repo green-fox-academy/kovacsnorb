ec87f8bbaacd11d1606da3d78eb927349b725091
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 17:14:20 +0200

    Week2/Day2 - 09-SumElements is done

---
+++ week-02/day-02/09-SumElements/09-SumElements/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09_SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `r`
+            //   with the following content: `[54, 23, 66, 12]`
+            // - Print the sum of the second and the third element
+            int[] r = new int[4] { 54, 23, 66, 12};
+            Console.WriteLine(r[1] + r[2]);
+            Console.ReadLine();
+        }
+    }
+}
