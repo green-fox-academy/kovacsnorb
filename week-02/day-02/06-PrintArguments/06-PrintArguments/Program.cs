58674feedb8e3f4bde75658a96279b0b0782c7c5
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 16:46:13 +0200

    Week2/Day2 - 07-Program is done (06-PrintArgument partially done)

---
+++ week-02/day-02/06-PrintArguments/06-PrintArguments/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06_PrintArguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+
+            string input01 = "kutya";
+
+
+
+            Printer(input01);
+            Console.ReadLine();
+        }
+
+        public static void Printer(params string[] givenString)
+        {
+
+        }
+
+
+    }
+}
