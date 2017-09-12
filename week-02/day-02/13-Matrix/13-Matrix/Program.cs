fc0964af5f52b106f623f0c2f645e8c9900ad5fb
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 22:48:42 +0200

    Week2/Day2 - 13-Matrix is done

---
+++ week-02/day-02/13-Matrix/13-Matrix/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _13_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+            int[,] matrix = new int[4, 4];
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        matrix[i, j] = 1;
+                    }
+                    else
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            int counter = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                    if(counter < 3)
+                    {
+                        Console.Write(matrix[i, j]);
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine(matrix[i, j]);
+                        counter = 0;
+                    }
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
