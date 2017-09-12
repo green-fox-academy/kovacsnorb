a8e77a35632672870716b71d9e2fd8dfba0356eb
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 13:52:55 +0200

    Week2/Day2 - 05-Factorial is done

---
+++ week-02/day-02/05-Factorial/05-Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+            Console.Write("Give me a number: ");
+            double number = double.Parse(Console.ReadLine());
+            Console.WriteLine(Factorio(number));
+            Console.ReadLine();
+        }
+
+        public static double Factorio(double param)
+        {
+            double multiplied = 1;
+            for (double i = param; i > 0; i--)
+            {
+                multiplied *= i;
+            }
+            return multiplied;
+        }
+    }
+}
