02fd88b2e4fc899b735e305b623fa3ef40507534
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 13:22:17 +0200

    Week2/Day2 - 02-GreeterFunction is done

---
+++ week-02/day-02/02-GreeterFunction/02-GreeterFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _02_GreeterFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+            string al = "Greenfox";
+            Greet(al);
+            Console.ReadLine();
+        }
+
+        public static void Greet(string name)
+        {
+            Console.WriteLine("Greetings dear, " + name);
+        }
+    }
+}
