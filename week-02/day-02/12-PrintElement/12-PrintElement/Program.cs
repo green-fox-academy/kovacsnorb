236b439bdcd8acd308b4d2e57a3b28b3698dcd14
kovacsnorb <kovacsnorb@gmail.com>
2017-09-12 17:31:34 +0200

    Week2/Day2 - 12-PrintElement is done

---
+++ week-02/day-02/12-PrintElement/12-PrintElement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _12_PrintElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+            int[] af = new int[4] { 4, 5, 6, 7 };
+            for (int i = 0; i < af.Length; i++)
+            {
+                Console.Write("{0}, ", af[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
