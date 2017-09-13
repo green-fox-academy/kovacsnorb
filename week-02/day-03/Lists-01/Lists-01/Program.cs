44f1720a81423776a2c3fd6ceca2cc51ee9cdc75
kovacsnorb <kovacsnorb@gmail.com>
2017-09-13 15:22:54 +0200

    Week2/Day3 - Lists-01 is done

---
+++ week-02/day-03/Lists-01/Lists-01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var planetList = new List<string> { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus",
+            "Neptune" };
+
+
+            // Saturn is missing from the planetList
+            // Insert it into the correct position
+
+            planetList.Insert(5, "Saturn");
+
+            for(int i = 0; i < planetList.Count; i++)
+            {
+                Console.WriteLine(planetList[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
