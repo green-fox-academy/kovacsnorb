4697b591606019c3779fac2506bc085a27f4e735
kovacsnorb <kovacsnorb@gmail.com>
2017-09-13 16:33:16 +0200

    Week2/Day3 - Lists-03 is done

---
+++ week-02/day-03/Lists-03/Lists-03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var far = new List<string> { "kuty", "macsk", "kacs", "rók", "halacsk" };
+            // Add "a" to every string in the far list.
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                far[i] = far[i] + "a";
+            }
+
+            for (int i = 0; i < far.Count; i++)
+            {
+                Console.Write("{0}, ", far[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
