de556f72e51db9dbb4b31698d4d1ecea6303c0d2
kovacsnorb <kovacsnorb@gmail.com>
2017-09-13 15:10:10 +0200

    Week2/Day3 - Strings-05 is done

---
+++ week-02/day-03/Strings-05/Strings-05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            Console.WriteLine(ReverseFunction(reversed));
+            Console.ReadLine();
+        }
+
+        public static string ReverseFunction(string input)
+        {
+            string reversedText = "";
+            for (int i=0; i < input.Length; i++)
+            {
+                reversedText = reversedText + input[input.Length - i - 1];
+            }
+            return reversedText;
+        }
+    }
+}
