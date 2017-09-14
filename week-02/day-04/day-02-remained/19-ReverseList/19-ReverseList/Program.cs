d1f1ef17a5b1dc247c5436b206cfa9e2327da32f
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 13:41:49 +0200

    Week2/Day4/Day-02-remained - 19-ReverseList is done

---
+++ week-02/day-04/day-02-remained/19-ReverseList/19-ReverseList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _19_ReverseList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            int[] aj = new int[5] { 3, 4, 5, 6, 7 };
+            int temp = 0;
+
+            for (int i = 0; i < aj.Length / 2; i++)
+            {
+                temp = aj[i];
+                aj[i] = aj[aj.Length - i - 1];
+                aj[aj.Length - i - 1] = temp;
+            }
+
+            for (int i = 0; i < aj.Length; i++)
+            {
+                Console.Write("{0}, ", aj[i]);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
