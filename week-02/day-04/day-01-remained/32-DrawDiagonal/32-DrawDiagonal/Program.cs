d2886df63aaed6a41dbdb81661cb992b3e070165
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 10:29:32 +0200

    Week2/Day4/Day-01-remained - 32-DrawDiagonal is done

---
+++ week-02/day-04/day-01-remained/32-DrawDiagonal/32-DrawDiagonal/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _32_DrawDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %%  %
+            // % % %
+            // %  %%
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+            Console.Write("Give me a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number - 1; i++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine("%");
+
+            int counter = 1;
+
+            for (int j = 0; j < number - 2; j++)
+            {
+                for (int i = 0; i < number - 1; i++)
+                {
+                    if (i == 0 || (i == counter))
+                    {
+                        Console.Write("%");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine("%");
+                counter++;
+            }
+
+            for (int i = 0; i < number - 1; i++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine("%");
+
+            Console.ReadLine();
+        }
+    }
+}
