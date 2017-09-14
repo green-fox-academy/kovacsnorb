c83e4e17b9353a2c1ecd8c4bf071068cf6c50650
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 10:02:10 +0200

    Week2/Day4/Day-01-remained - 20-PrintBigger is done

---
+++ week-02/day-04/day-01-remained/20-PrintBigger/20-PrintBigger/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _20_PrintBigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            Console.Write("Give me number1: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Give me number2: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (number1 == number2)
+            {
+                Console.WriteLine("The two number is equal.");
+            }
+            else if (number2 > number1)
+            {
+                Console.WriteLine("Number2 is the bigger.");
+            }
+            else
+            {
+                Console.WriteLine("Number1 is the bigger.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
