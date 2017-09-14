1014d6cde0b66bd7b16e80f38273aff7dde55d6e
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 11:03:03 +0200

    Week2/Day4/Day-01-remained - 34-ParametricAverage is done

---
+++ week-02/day-04/day-01-remained/33-ParametricAverage/33-ParametricAverage/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _33_ParametricAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+            Console.Write("Give me a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            double sum = 0.0;
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("Give me number " + (i+1) + ": ");
+                int givenNumber = int.Parse(Console.ReadLine());
+                sum += givenNumber;
+                counter++;
+            }
+            Console.WriteLine("Sum: " + sum + ", Average: " + (sum/counter));
+            Console.ReadLine();
+        }
+    }
+}
