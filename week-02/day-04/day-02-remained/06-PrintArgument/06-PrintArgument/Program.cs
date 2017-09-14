239d10d065d83ecf6543bb6ab8b5c61c18031594
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 15:18:58 +0200

    Week2/Day4/Day-02-remained - 06-PrintArgument is done

---
+++ week-02/day-04/day-02-remained/06-PrintArgument/06-PrintArgument/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06_PrintArgument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+            string[] text = new string[8] { "elso", "masodik", "harmadik", "negyedik", "otodik", "hatodik", "hetedik", "nyolcadik" };
+            
+            Printer(text);
+            Console.ReadLine();
+        }
+
+        public static void Printer(string[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.Write("{0}, ", input[i]);
+            }
+
+        }
+
+    }
+}
