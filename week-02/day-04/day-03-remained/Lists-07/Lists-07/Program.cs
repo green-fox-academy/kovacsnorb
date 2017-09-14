6ae88706257958f431add480d4f22a1fadc2b68c
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 15:38:27 +0200

    Week2/Day4/Day-03-remained - Lists-07 is done

---
+++ week-02/day-04/day-03-remained/Lists-07/Lists-07/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<string> { "What", "I", "do", "create,", "I", "cannot", "not", "understand." };
+
+            // Accidentally I messed up this quote from Richard Feynman.
+            // Two words are out of place
+            // Your task is to fix it by swapping the right words with code
+
+            // Also, print the sentence to the output with spaces in between.
+
+            string temp = list[2];
+            list[2] = list[5];
+            list[5] = temp;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write("{0} ", list[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
