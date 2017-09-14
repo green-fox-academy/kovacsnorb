d6e6a6f09d5e3d92b2111e941c534d59b22891cf
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 10:14:57 +0200

    Week2/Day4/Day-01-remained - 31-DrawSquare is done

---
+++ week-02/day-04/day-01-remained/31-DrawSquare/31-DrawSquare/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _31_DrawSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+
+            Console.Write("Give me a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number - 1; i++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine("%");
+
+            for (int i = 0; i < number - 2; i++)
+            {
+                Console.Write("%");
+                for (int j = 0; j < number - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("%");
+
+            }
+
+            for (int i = 0; i < number - 1; i++)
+            {
+                Console.Write("%");
+            }
+            Console.WriteLine("%");
+ 
+            Console.ReadLine();
+        }
+    }
+}
