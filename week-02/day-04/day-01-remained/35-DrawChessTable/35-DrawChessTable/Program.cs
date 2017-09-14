aceed95930ec5906cf697cf5db1eb5831fd1f8e6
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 11:54:08 +0200

    Week2/Day4/Day-01-remained - 35-DrawChessTable is done

---
+++ week-02/day-04/day-01-remained/35-DrawChessTable/35-DrawChessTable/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _35_DrawChessTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crate a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+            int sizeOfTable = 8;
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int i = 0; i < sizeOfTable - 1; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write("%");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine(" ");
+
+                for (int i = 0; i < sizeOfTable - 1; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("%");
+                    }
+                }
+                Console.WriteLine("%");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
