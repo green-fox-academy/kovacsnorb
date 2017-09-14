b4b802fd08b36c5eb2fc6408e1b13c0acb972352
kovacsnorb <kovacsnorb@gmail.com>
2017-09-14 10:47:11 +0200

    Week2/Day4/Day-01-remained - 33-GuessTheNumber is done

---
+++ week-02/day-04/day-01-remained/33-GuessTheNumber/33-GuessTheNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _33_GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores a number, and the user has to figure it out.
+            // The user can input guesses, after each guess the program would tell one
+            // of the following:
+            //
+            // The stored number is higher
+            // The stried number is lower
+            // You found the number: 8
+
+            int storedNumber = 10;
+            int userNumber = 0;
+
+            Console.Write("Give me a number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            while (userNumber != storedNumber)
+            {
+                if (userNumber > storedNumber)
+                {
+                    Console.WriteLine("The stored number is lower.");
+                }
+                else
+                {
+                    Console.WriteLine("The stored number is higher.");
+                }
+                Console.Write("Give me another number: ");
+                userNumber = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("You found the number: " + storedNumber);
+            Console.ReadLine();
+        }
+
+    }
+}
