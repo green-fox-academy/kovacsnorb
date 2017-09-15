a7cddaef980a97364d21183c25eb7e101253782d
kovacsnorb <kovacsnorb@gmail.com>
2017-09-15 10:48:43 +0200

    Week2/Day5 - Anagram is done

---
+++ week-02/day-05/Anagram/Anagram/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me word one: ");
+            string wordOne = Console.ReadLine();
+            Console.Write("Give me word two: ");
+            string wordTwo = Console.ReadLine();
+
+            char[] listOne = FillTheList(wordOne);
+            char[] listTwo = FillTheList(wordTwo);
+
+            Array.Sort(listOne);
+            Array.Sort(listTwo);
+
+            bool isAnagram;
+
+            if (listOne.Length == listTwo.Length)
+            {
+                int counter = 0;
+
+                for (int i = 0; i < listOne.Length; i++)
+                {
+                    if (listOne[i] != listTwo[i])
+                    {
+                        counter++;
+                    }
+                }
+
+                if (counter == 0)
+                {
+                    isAnagram = true;
+                }
+                else
+                {
+                    isAnagram = false;
+                }
+            }
+            else
+            {
+                isAnagram = false;
+            }
+
+            Console.WriteLine(isAnagram);
+            Console.ReadLine();
+        }
+
+        public static char[] FillTheList(string userInput)
+        {
+            char[] inputSplit = userInput.ToCharArray();
+            return inputSplit;
+        }
+    }
+}
