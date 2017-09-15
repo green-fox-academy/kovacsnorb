8178fd6ba53322d9f04691c6e2bdec490bf12da1
kovacsnorb <kovacsnorb@gmail.com>
2017-09-15 11:04:49 +0200

    Week2/Day5 - PalindromBuilder is done

---
+++ week-02/day-05/PalindromBuilder/PalindromBuilder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PalindromBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a word: ");
+            string userInput = Console.ReadLine();
+            char[] inputToChar = userInput.ToCharArray();
+            string reverseUserInput = "";
+
+            for (int i = 0; i < inputToChar.Length; i++)
+            {
+                reverseUserInput += inputToChar[inputToChar.Length - i - 1];
+            }
+
+            Console.WriteLine(userInput + reverseUserInput);
+            Console.ReadLine();
+        }
+    }
+}
