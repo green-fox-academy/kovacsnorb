3ed3474b4ded5cc962f0272a719eef534a1df371
kovacsnorb <kovacsnorb@gmail.com>
2017-09-17 23:05:20 +0200

    Week2/Day5 - ArmstrongNumber is done

---
+++ week-02/day-05/ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ArmstrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give me a number to check: ");
+            double userInput = double.Parse(Console.ReadLine());
+            int length = userInput.ToString().Length;
+
+            double userInputDuplication = userInput;
+            double sum = 0;
+            double toAdd = 0;
+            double toSub = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                toAdd = Math.Pow(Math.Truncate((userInputDuplication / Math.Pow(10, length - 1 - i))), length);
+                toSub = Math.Truncate((userInputDuplication / Math.Pow(10, length - 1 - i))) * Math.Pow(10, length - 1 - i);
+                sum += toAdd;
+                userInputDuplication -= toSub;
+            }
+
+            if (sum == userInput)
+            {
+                Console.WriteLine("The " + userInput + " is an Armstrong number. The calculated value is: " + sum + ".");
+            }
+            else
+            {
+                Console.WriteLine("The " + userInput + " is NOT an Armstrong number. The calculated value is: " + sum + ".");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
