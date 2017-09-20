60646998415ace043757a7683075bd82e8cf1547
kovacsnorb <kovacsnorb@gmail.com>
2017-09-20 13:45:16 +0200

    Week 03/Day-03 - (04 Go to center) is done

---
+++ week-03/day-03/04-GoToCenter/04-GoToCenter/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _04_GoToCenter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FoxDraw foxDraw = new FoxDraw(canvas);
+            foxDraw.BackgroundColor(Colors.PeachPuff);
+            // create a line drawing function that takes 2 parameters:
+            // the x and y coordinates of the line's starting point
+            // and draws a line from that point to the center of the canvas.
+            // draw 3 lines with that function.
+            LineToCenter(foxDraw, 10, 20);
+            LineToCenter(foxDraw, 40, 125);
+            LineToCenter(foxDraw, 205, 8);
+        }
+
+        public void LineToCenter(FoxDraw foxDraw, double xCoordinate, double yCoordinate)
+        {
+            foxDraw.StrokeColor(Colors.Blue);
+            Point endPoint = new Point(canvas.Width / 2, canvas.Height / 2);
+            Point startPoint = new Point(xCoordinate, yCoordinate);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+
+    }
+}
