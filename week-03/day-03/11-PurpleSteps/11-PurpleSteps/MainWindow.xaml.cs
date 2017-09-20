52e0c9c7a99ccad10f9865c55ed11f54c7f33b5d
kovacsnorb <kovacsnorb@gmail.com>
2017-09-20 16:03:36 +0200

    Week 03/Day-03 - (11 Purple Steps) is done

---
+++ week-03/day-03/11-PurpleSteps/11-PurpleSteps/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _11_PurpleSteps
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FoxDraw foxDraw = new FoxDraw(canvas);
+            foxDraw.BackgroundColor(Colors.Yellow);
+            // reproduce this:
+            // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
+
+            int numberOfLoops = 20;
+            double sizeOfSquares = 10;
+
+            for (int i = 1; i < numberOfLoops; i++)
+            {
+                DrawSquares(foxDraw, sizeOfSquares, i);
+            }
+        }
+
+        public void DrawSquares(FoxDraw foxDraw, double size, int startMultiplier)
+        {
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.FillColor(Colors.MediumPurple);
+            foxDraw.DrawRectangle(startMultiplier * size, startMultiplier * size, size, size);
+        }
+    }
+}
