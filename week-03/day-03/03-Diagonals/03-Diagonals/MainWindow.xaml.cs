bd9666a18c34d0026e2f3e273b1763cb7d6d108f
kovacsnorb <kovacsnorb@gmail.com>
2017-09-20 13:17:42 +0200

    Week 03/Day-03 - (03 Diagonals) is done

---
+++ week-03/day-03/03-Diagonals/03-Diagonals/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace _03_Diagonals
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FoxDraw foxDraw = new FoxDraw(canvas);
+            // draw the canvas' diagonals in green.
+
+            Point upperLeft = new Point(0, 0);
+            Point upperRight = new Point(0, canvas.Width);
+            Point downLeft = new Point(canvas.Height, 0);
+            Point downRight = new Point(canvas.Height, canvas.Width);
+
+            DiagonalsInGreen(foxDraw, upperLeft, downRight);
+            DiagonalsInGreen(foxDraw, downLeft, upperRight);
+
+            foxDraw.BackgroundColor(Colors.LightCoral);
+        }
+
+        public static void DiagonalsInGreen(FoxDraw foxDraw, Point startPoint, Point endPoint)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(startPoint, endPoint);
+        }
+    }
+}
