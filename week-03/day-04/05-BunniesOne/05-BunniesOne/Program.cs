ffa73d21de34aebcf6fad1700b3dbb3cec987331
kovacsnorb <kovacsnorb@gmail.com>
2017-09-21 11:37:33 +0200

    Week 03/Day-04 - (05 Bunnies One) is done

---
+++ week-03/day-04/05-BunniesOne/05-BunniesOne/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_BunniesOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+            Console.Write("How many bunnies do we have? ");
+            int numberOfBunnies = int.Parse(Console.ReadLine());
+            Console.WriteLine(BunniesOne(numberOfBunnies));
+            Console.ReadLine();
+        }
+
+        public static int BunniesOne(int userInput)
+        {
+            if (userInput == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                return 2 + BunniesOne(userInput - 1);
+            }
+        }
+    }
+}
