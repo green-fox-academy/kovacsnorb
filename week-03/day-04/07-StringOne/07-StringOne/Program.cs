5e780f512faf33391e9aad26a7a4b6f4c5726fca
kovacsnorb <kovacsnorb@gmail.com>
2017-09-21 13:54:29 +0200

    Week 03/Day-04 - (07 String One) is done

---
+++ week-03/day-04/07-StringOne/07-StringOne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_StringOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+
+            Console.Write("Give me a string: ");
+            string input = Console.ReadLine();
+            int inputLength = input.Length;
+            Console.WriteLine((CharacterChange(input, inputLength)));
+            Console.ReadLine();
+        }
+
+        public static string CharacterChange(string input, int length)
+        {
+
+            string result;
+            if (length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (input.Substring(length - 1, 1) == "x")
+                {
+                    result = "y";
+
+                    return CharacterChange(input, length - 1) + result;
+                }
+                else
+                {
+                    return CharacterChange(input, length - 1) + input.Substring(length - 1, 1);
+                }
+            }
+        }
+    }
+}
---
+++ week-03/day-04/Test2/Test2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "tregwwwretgetgscxwxss";
+            string test2 = test.Replace('w', 'y');
+            Console.WriteLine(test);
+            Console.WriteLine(test2);
+            Console.ReadLine();
+        }
+    }
+}
