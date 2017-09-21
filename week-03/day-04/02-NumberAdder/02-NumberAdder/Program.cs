22476e8f3254b8e06e03a38e22ce9742fc03014d
kovacsnorb <kovacsnorb@gmail.com>
2017-09-21 10:44:11 +0200

    Week 03/Day-04 - (02 Number Adder) is done

---
+++ week-03/day-04/02-NumberAdder/02-NumberAdder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
+            Console.Write("Give me a number and I sum numbers from 1 to your number: ");
+            int userInput = int.Parse(Console.ReadLine());
+            Console.WriteLine(NumberAdder(userInput));
+            Console.ReadLine();
+        }
+
+        public static int NumberAdder(int input)
+        {
+            if (input == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return input + NumberAdder(input - 1);
+            }
+        }
+    }
+}
