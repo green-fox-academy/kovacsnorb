9ed11b70c1a3b1467b828a55fd0dbe1c1f92f12b
kovacsnorb <kovacsnorb@gmail.com>
2017-09-21 10:33:32 +0200

    Week 03/Day-04 - (01 Counter) is done

---
+++ week-03/day-04/01-Counter/01-Counter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a recursive function that takes one parameter: n and counts down from n.
+            Console.Write("Give me a number: ");
+            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine(Counter(input));
+            Console.ReadLine();
+        }
+
+        public static int Counter(int input)
+        {
+            if (input == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine(input);
+                input -= 1;
+                return Counter(input);
+            }
+        }
+    }
+}
---
+++ week-03/day-04/Test/Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FactorialWithLoop(10));
+            Console.WriteLine(FactorialWithRecursion(10));
+            Console.ReadLine();
+        }
+
+        public static int FactorialWithLoop(int n)
+        {
+            int factorial = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        public static int FactorialWithRecursion(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * FactorialWithRecursion(n - 1);
+            }
+        }
+    }
+}
