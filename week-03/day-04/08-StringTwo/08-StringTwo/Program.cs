3a0945a76d3c0374bd1ad47199cb88a63f3c2fa7
kovacsnorb <kovacsnorb@gmail.com>
2017-09-21 13:59:17 +0200

    Week 03/Day-04 - (08 String Again) is done

---
+++ week-03/day-04/08-StringTwo/08-StringTwo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_StringTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+            Console.Write("Give me a string: ");
+            string input = Console.ReadLine();
+            int inputLength = input.Length;
+            Console.WriteLine((CharacterChange(input, inputLength)));
+            Console.ReadLine();
+        }
+
+        public static string CharacterChange(string input, int length)
+        {
+
+            string result;
+            if (length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (input.Substring(length - 1, 1) == "x")
+                {
+                    result = "";
+
+                    return CharacterChange(input, length - 1) + result;
+                }
+                else
+                {
+                    return CharacterChange(input, length - 1) + input.Substring(length - 1, 1);
+                }
+            }
+        }
+    }
+}
