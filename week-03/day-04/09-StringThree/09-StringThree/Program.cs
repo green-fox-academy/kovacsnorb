951e79bd1dd45ddc88aa98ac74966b3ea3aedfa8
kovacsnorb <kovacsnorb@gmail.com>
2017-09-21 15:51:39 +0200

    Week 03/Day-04 - (09 String Again And Again) is done

---
+++ week-03/day-04/09-StringThree/09-StringThree/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_StringThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively a new string where all the
+            // adjacent chars are now separated by a "*".
+
+            Console.Write("Give me a string: ");
+            string input = Console.ReadLine();
+            int inputLength = input.Length;
+            Console.WriteLine((CharacterChange(input, inputLength)));
+            Console.ReadLine();
+        }
+
+        public static string CharacterChange(string input, int length)
+        {
+            //if (length == 0)
+            //{
+            //    return "*";
+            //}
+            //else
+            //{
+            //    {
+            //        return CharacterChange(input, length - 1) + input.Substring(length - 1, 1) + "*";
+            //    }
+            //}
+
+            return length == 0 ? "*" : CharacterChange(input, length - 1) + input.Substring(length - 1, 1) + "*";
+        }
+    }
+}
