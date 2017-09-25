21216cb5ee86ec4f189054e6227738e57718df10
kovacsnorb <kovacsnorb@gmail.com>
2017-09-25 13:15:02 +0200

    W04/D01 - (01 Post it) is done

---
+++ week-04/day-01/01-PostIt/01-PostIt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _01_PostIt
+{
+    class Program
+    {
+        public struct PostIt
+        {
+            public string BackgoundColor;
+            public string Text;
+            public string TextColor;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            PostIt postIt1;
+            PostIt postIt2;
+            PostIt postIt3;
+
+            postIt1.BackgoundColor = "orange";
+            postIt1.TextColor = "blue";
+            postIt1.Text = "Idea 1";
+
+            postIt2.BackgoundColor = "pink";
+            postIt2.TextColor = "black";
+            postIt2.Text = "Awsome";
+
+            postIt3.BackgoundColor = "yellow";
+            postIt3.TextColor = "green";
+            postIt3.Text = "Superb";
+
+            Console.WriteLine(postIt1.BackgoundColor + ", " + postIt1.TextColor + ", " + postIt1.Text);
+            Console.WriteLine(postIt2.BackgoundColor + ", " + postIt2.TextColor + ", " + postIt2.Text);
+            Console.WriteLine(postIt3.BackgoundColor + ", " + postIt3.TextColor + ", " + postIt3.Text);
+
+            Console.ReadLine();
+        }
+    }
+}
