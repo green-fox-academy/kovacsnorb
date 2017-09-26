290fd5f8814af587b2f7bf263f792d7eddf37fc2
kovacsnorb <kovacsnorb@gmail.com>
2017-09-26 16:03:54 +0200

    W04/D02 - (01 GreenFoxOrganization) Person.cs is done

---
+++ week-04/day-02/01-GreenFoxOrganization/01-GreenFoxOrganization/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_GreenFoxOrganization
+{
+    class Person
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Person(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+        }
+
+        public Person()
+        {
+            this.name = "Jane Doe";
+            this.age = 30;
+            this.gender = "female";
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I am {0}, a {1} year old {2}.", this.name, this.age, this.gender);
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+    }
+}
---
+++ week-04/day-02/01-GreenFoxOrganization/01-GreenFoxOrganization/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_GreenFoxOrganization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person("Reka", 20, "female");
+
+            person1.Introduce();
+
+            Console.ReadLine();
+        }
+    }
+}
