2acfc8306ed0e9265be11508b545c4e820b43066
kovacsnorb <kovacsnorb@gmail.com>
2017-09-27 11:44:51 +0200

    W04/D03 - (01 Apples) is finished

---
+++ week-04/day-03/01_Apples/AppleTest/AppleTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Apples;
+
+namespace ApplesTest
+{
+    [TestFixture]
+
+    public class AppleTest
+    {
+        Apple apple = new Apple();
+
+        [Test]
+        public void TestApple_WhenApple_ThenApple()
+        {
+            string output = "apple";
+            Assert.AreEqual(output, apple.GetApple());
+        }
+
+        [Test]
+        public void TestApple_WhenApples_ThenFailedTest()
+        {
+            string output = "apples";
+            Assert.AreEqual(output, apple.GetApple());
+        }
+    }
+}
