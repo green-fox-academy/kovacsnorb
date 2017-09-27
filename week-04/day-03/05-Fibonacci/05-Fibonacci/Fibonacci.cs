aa870b083a3ff70ce4d55ff4058d85de20ba8059
kovacsnorb <kovacsnorb@gmail.com>
2017-09-27 16:55:57 +0200

    W04/D03 - (05 Fibonacci) is finished

---
+++ week-04/day-03/05-Fibonacci/05-Fibonacci/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Fibonacci
+{
+    public class Fibonacci
+    {
+        public int FibonacciCalculator(int nthFibo)
+        {
+            if (nthFibo < 1)
+            {
+                Console.WriteLine("Give me a number >= 1.");
+                return -1;
+            }
+            else if (nthFibo == 1)
+            {
+                return 0;
+            }
+            else if (nthFibo == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                int previousMinusOne = 0;
+                int previous = 1;
+                int resultOfFibonacci = 0;
+
+                for (int i = 0; i < nthFibo - 2; i++)
+                {
+                    resultOfFibonacci = previousMinusOne + previous;
+                    previousMinusOne = previous;
+                    previous = resultOfFibonacci;
+                }
+                return resultOfFibonacci;
+            }
+        }
+    }
+}
---
+++ week-04/day-03/05-Fibonacci/FibonacciTest/TestCases.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _05_Fibonacci;
+using NUnit.Framework;
+
+namespace FibonacciTest
+{
+    [TestFixture]
+    public class TestCases
+    {
+        Fibonacci fibonacci = new Fibonacci();
+
+        [Test]
+        public void TestFibonacci_When11_Then55()
+        {
+            int input = 11;
+            int output = 55;
+            Assert.AreEqual(output, fibonacci.FibonacciCalculator(input));
+        }
+
+        [Test]
+        public void TestFibonacci_When1_Then0()
+        {
+            int input = 1;
+            int output = 0;
+            Assert.AreEqual(output, fibonacci.FibonacciCalculator(input));
+        }
+
+        [Test]
+        public void TestFibonacci_When2_Then1()
+        {
+            int input = 2;
+            int output = 1;
+            Assert.AreEqual(output, fibonacci.FibonacciCalculator(input));
+        }
+
+        [Test]
+        public void TestFibonacci_When3_Then1()
+        {
+            int input = 3;
+            int output = 1;
+            Assert.AreEqual(output, fibonacci.FibonacciCalculator(input));
+        }
+
+        [Test]
+        public void TestFibonacci_When4_Then2()
+        {
+            int input = 4;
+            int output = 2;
+            Assert.AreEqual(output, fibonacci.FibonacciCalculator(input));
+        }
+
+        [Test]
+        public void TestFibonacci_WhenMinus5_ThenMinus1()
+        {
+            int input = -5;
+            int output = -1;
+            Assert.AreEqual(output, fibonacci.FibonacciCalculator(input));
+        }
+    }
+}
