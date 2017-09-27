9f6f59f9578fc2ba01d35c256db1f86a00e2e73b
kovacsnorb <kovacsnorb@gmail.com>
2017-09-27 13:35:26 +0200

    W04/D03 - (02 Sum) is finished

---
+++ week-04/day-03/02-Sum/02-Sum/Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Sum
+{
+    public class Sum
+    {
+        public int SumOfRecords(List<int> inputList)
+        {
+            int sumOfElements = 0;
+
+            if (inputList != null)
+            {
+                foreach (int element in inputList)
+                {
+                    sumOfElements += element;
+                }
+            }
+            
+            return sumOfElements;
+        }
+    }
+}
---
+++ week-04/day-03/02-Sum/SumTest/SumTestCases.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _02_Sum;
+using NUnit.Framework;
+
+namespace SumTest
+{
+    [TestFixture]
+
+    public class SumTestCases
+    {
+        Sum sum = new Sum();
+
+        [Test]
+        public void SumTest_WhenList_4_5_6_9_Then24()
+        {
+            List<int> input = new List<int> { 4, 5, 6, 9 };
+            int output = 24;
+
+            Assert.AreEqual(output, sum.SumOfRecords(input));
+        }
+
+        [Test]
+        public void SumTest_WhenList_4_5_6_9_17_Then41()
+        {
+            List<int> input = new List<int> { 4, 5, 6, 9, 17 };
+            int output = 41;
+
+            Assert.AreEqual(output, sum.SumOfRecords(input));
+        }
+
+        [Test]
+        public void SumTest_WhenOneElement230_Then230()
+        {
+            List<int> input = new List<int> { 230 };
+            int output = 230;
+
+            Assert.AreEqual(output, sum.SumOfRecords(input));
+        }
+
+        [Test]
+        public void SumTest_WhenEmptyList_Then0()
+        {
+            List<int> input = new List<int> { };
+            int output = 0;
+
+            Assert.AreEqual(output, sum.SumOfRecords(input));
+        }
+
+        [Test]
+        public void SumTest_WhenNullList_Then0()
+        {
+            List<int> input = null;
+            int output = 0;
+
+            Assert.AreEqual(output, sum.SumOfRecords(input));
+        }
+    }
+}
