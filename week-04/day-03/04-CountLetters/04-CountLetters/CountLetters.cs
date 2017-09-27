366a0aeefb7a3b990fc32a625ee7ebb63fad21e1
kovacsnorb <kovacsnorb@gmail.com>
2017-09-27 15:14:28 +0200

    W04/D03 - (04 CountLetters) is finished

---
+++ week-04/day-03/04-CountLetters/04-CountLetters/CountLetters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_CountLetters
+{
+    public class CountLetters
+    {
+        public Dictionary<char, int> LetterCounter(string input)
+        {
+            char[] inputArray = input.ToCharArray();
+            Array.Sort(inputArray);
+
+            var resultDictionary = new Dictionary<char, int>();
+
+            char tempChar;
+            char previousTempChar = ' ';
+            int tempCounter = 0;
+
+            for (int i = 0; i < inputArray.Count(); i++)
+            {
+                tempChar = inputArray[i];
+
+                if (tempChar != previousTempChar)
+                {
+                    foreach (char character in inputArray)
+                    {
+                        if (character == tempChar)
+                        {
+                            tempCounter++;
+                        }
+                    }
+                    resultDictionary.Add(tempChar, tempCounter);
+                    tempCounter = 0;
+                    previousTempChar = tempChar;
+                }
+            }
+            return resultDictionary;
+        }
+    }
+}
---
+++ week-04/day-03/04-CountLetters/TestCountLetters/TestCases.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _04_CountLetters;
+using NUnit.Framework;
+
+namespace TestCountLetters
+{
+    [TestFixture]
+
+    public class TestCases
+    {
+        CountLetters countLetters = new CountLetters();
+
+        [Test]
+        public void CountLettersTest_WithWordAlmafa()
+        {
+            string input = "almafa";
+            Dictionary<char, int> output = new Dictionary<char, int>
+            {
+                { 'a', 3 },
+                { 'f', 1 },
+                { 'l', 1 },
+                { 'm', 1 }
+            };
+            Assert.AreEqual(output, countLetters.LetterCounter(input));
+        }
+    }
+}
