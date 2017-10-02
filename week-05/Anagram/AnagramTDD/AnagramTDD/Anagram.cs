0deb67af83c295d7734661f681bf724e50aa769b
kovacsnorb <kovacsnorb@gmail.com>
2017-10-02 16:54:41 +0200

    Anagram TDD

---
+++ week-05/Anagram/AnagramTDD/AnagramTDD/Anagram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramTDD
+{
+    public class Anagram
+    {
+
+        public static bool IsAnagram(string input1, string input2)
+        {
+
+            input1 = input1.ToLower();
+            input2 = input2.ToLower();
+
+            char[] CharArray1 = input1.ToCharArray();
+            List<char> list1 = CharArray1.ToList();
+            for (int i = 0; i < list1.Count; i++)
+            {
+                if (list1[i] == ' ')
+                {
+                    list1.RemoveAt(i);
+                }
+            }
+            list1.Sort();
+            char[] CharArray2 = input2.ToCharArray();
+            List<char> list2 = CharArray2.ToList();
+            for (int i = 0; i < list2.Count; i++)
+            {
+                if (list2[i] == ' ')
+                {
+                    list2.RemoveAt(i);
+                }
+            }
+            list2.Sort();
+
+            if (list1.Count != list2.Count)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < list1.Count; i++)
+                {
+                    if (list1[i] != list2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ week-05/Anagram/AnagramTDD/AnagramTest/Tests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnagramTDD;
+using NUnit.Framework;
+
+namespace AnagramTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestAnagram_SameWords()
+        {
+            string input1 = "alma";
+            string input2 = "alma";
+
+            Assert.AreEqual(true, Anagram.IsAnagram(input1, input2));
+
+            input1 = "korte";
+            input2 = "korte";
+
+            Assert.AreEqual(true, Anagram.IsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagram_DifferentWords()
+        {
+            string input1 = "alma";
+            string input2 = "korte";
+
+            Assert.AreEqual(false, Anagram.IsAnagram(input1, input2));
+
+            input1 = "alma";
+            input2 = "malna";
+
+            Assert.AreEqual(false, Anagram.IsAnagram(input1, input2));
+
+            input1 = "korte";
+            input2 = "malna";
+
+            Assert.AreEqual(false, Anagram.IsAnagram(input1, input2));
+        }
+
+        [Test]
+        public void TestAnagram_SameWordsUpperLowerCase()
+        {
+            string input1 = "alma";
+            string input2 = "ALMA";
+
+            Assert.AreEqual(true, Anagram.IsAnagram(input1, input2));
+
+            input1 = "ALMA";
+            input2 = "alma";
+
+            Assert.AreEqual(true, Anagram.IsAnagram(input1, input2));
+
+        }
+        [Test]
+        public void TestAnagram_SameCharacter()
+        {
+            string input1 = "alma";
+            string input2 = "mala";
+
+            Assert.AreEqual(true, Anagram.IsAnagram(input1, input2));
+
+        }
+        [Test]
+        public void TestAnagram_WithEmptyString()
+        {
+            string input1 = String.Empty;
+            string input2 = "mala";
+
+            Assert.AreEqual(false, Anagram.IsAnagram(input1, input2));
+
+        }
+        [Test]
+        public void TestAnagram_WithWhiteSpace()
+        {
+            string input1 = "alma";
+            string input2 = "al ma";
+
+            Assert.AreEqual(true, Anagram.IsAnagram(input1, input2));
+        }
+    }
+}
