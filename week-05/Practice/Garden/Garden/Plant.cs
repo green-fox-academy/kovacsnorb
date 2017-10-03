815987ccbff8bbebc9ac83a622c0704c9deec878
kovacsnorb <kovacsnorb@gmail.com>
2017-10-03 19:23:46 +0200

    Garden - Plant class is done

---
+++ week-05/Practice/Garden/Garden/Plant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garden
+{
+    class Plant
+    {
+        string Name;
+        float ActualWaterLevel;
+        float Absorb;
+        float MinWaterLevel;
+
+        public Plant(string name)
+        {
+            Name = name;
+        }
+
+        public bool IsWaterNeeded()
+        {
+            return ActualWaterLevel < MinWaterLevel;
+        }
+
+        public void Water(float waterAmount)
+        {
+            ActualWaterLevel += waterAmount * Absorb;
+        }
+    }
+}
