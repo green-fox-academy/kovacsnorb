14787f6adc0b68b164636892511f1db54cb4d4f7
kovacsnorb <kovacsnorb@gmail.com>
2017-10-18 15:35:32 +0200

    W6/D3 Exercise 07 is done

---
+++ week-06/day-03/Exercise-07/Exercise-07/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a LINQ Expression to find the strings which starts with A and ends with I in the following array:
+
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            List<string> myResults = FrequencyOfChars(cities);
+            List<string> myResults2 = FrequencyOfCharsWithLambda(cities);
+
+            Console.ReadLine();
+        }
+
+        public static List<string> FrequencyOfChars(string[] inputArray)
+        {
+            var aiWords =
+                from strings in inputArray
+                where strings[0] == 'A' && strings[strings.Length - 1] == 'I' 
+                select strings;
+
+            return aiWords.ToList();
+        }
+
+        public static List<string> FrequencyOfCharsWithLambda(string[] inputArray)
+        {
+            var aiWords = inputArray.Where(x => x.StartsWith("A") && x.EndsWith("I"));
+
+            return aiWords.ToList();
+        }
+    }
+}
