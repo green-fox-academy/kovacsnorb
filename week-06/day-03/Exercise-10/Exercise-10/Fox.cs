5717a0c30271df9c2aa55e987b60e233694faf64
kovacsnorb <kovacsnorb@gmail.com>
2017-10-19 10:49:08 +0200

    W6/D3 Exercise 10 is done

---
+++ week-06/day-03/Exercise-10/Exercise-10/Fox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    public class Fox
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Colour { get; set; }
+
+        public Fox(string name, string type, string colour)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Colour = colour;
+        }
+
+
+    }
+}
---
+++ week-06/day-03/Exercise-10/Exercise-10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a Fox class with 3 properties(name, type, color) Fill a list with at least 5 foxes, 
+            //it's up to you how you name/create them! Write a LINQ Expression to find the foxes with green 
+            //color! Write a LINQ Expression to find the foxes with green color and pallida type!
+
+            var foxList = new List<Fox>();
+
+            foxList.Add(new Fox("Vuk", "fennec", "pink"));
+            foxList.Add(new Fox("Karak", "arctic", "yellow"));
+            foxList.Add(new Fox("Sanyi", "pallida", "grey"));
+            foxList.Add(new Fox("Joci", "pallida", "green"));
+            foxList.Add(new Fox("Julika", "pallida", "green"));
+            foxList.Add(new Fox("Sarkika", "pale", "green"));
+
+            var greenFoxList = FindMyGreenFoxes(foxList);
+            var greenPallidaList = FindMyGreenPallidas(foxList);
+
+            Console.ReadLine();
+        }
+
+        public static List<Fox> FindMyGreenFoxes(List<Fox> inputList)
+        {
+            return inputList.Where(fox => fox.Colour == "green").ToList();
+        }
+
+        public static List<Fox> FindMyGreenPallidas(List<Fox> inputList)
+        {
+            return inputList.Where(fox => fox.Colour == "green" && fox.Type == "pallida").ToList();
+        }
+    }
+}
