c071a994dbf36acb71d184d57c77578d3e6e9046
kovacsnorb <kovacsnorb@gmail.com>
2017-10-19 14:50:33 +0200

    W6/D4 Parking lot is done

---
+++ week-06/day-04/ParkingLot/ParkingLot/Cars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    class Cars
+    {
+        public string CarType { get; set; }
+        public string CarColor { get; set; }
+        private static readonly Random rnd = new Random();
+
+        public Cars()
+        {
+            this.CarType = ((CarTypes)rnd.Next(0, 3)).ToString();
+            this.CarColor = ((CarColors)rnd.Next(0, 3)).ToString();
+        }
+
+        public enum CarTypes
+        {
+            Opel,
+            Lada,
+            Fiat
+        }
+
+        public enum CarColors
+        {
+            Black,
+            White,
+            Green
+        }
+    }
+}
---
+++ week-06/day-04/ParkingLot/ParkingLot/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myParkingLot = FillListWithCars();
+            var countTypeFrequency = myParkingLot.GroupBy(x => x.CarType).ToDictionary(x => x.Key, x => x.Count());
+            var countColorFrequency = myParkingLot.GroupBy(x => x.CarColor).ToDictionary(x => x.Key, x => x.Count());
+
+            var myConcate = new List<string>();
+
+            foreach (Cars car in myParkingLot)
+            {
+                myConcate.Add(car.CarColor + " " + car.CarType);
+            }
+
+            var concateList = myConcate.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+            var mostFrequentCarOnly = concateList.FirstOrDefault(x => x.Value == concateList.Values.Max()).Key;
+
+            Console.WriteLine($"Most frequent car is {mostFrequentCarOnly} with {concateList.Values.Max()} cars.\n");
+
+            foreach (var listelement in concateList)
+            {
+                Console.WriteLine(listelement);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static List<Cars> FillListWithCars()
+        {
+            List<Cars> myList = new List<Cars>();
+
+            do
+            {
+                myList.Add(new Cars());
+            } while (myList.Count < 256);
+
+            return myList;
+        }
+    }
+}
