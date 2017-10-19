3e7817a701982f0057669538c8f209daca1b61ec
kovacsnorb <kovacsnorb@gmail.com>
2017-10-19 16:21:03 +0200

    W6/D4 Twenty-one - Deck is filled with Cards

---
+++ week-06/day-04/TwentyOne/TwentyOne/Card.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Card
+    {
+        private static readonly Random rnd = new Random();
+
+        public static Colors Color { get; set; }
+        public static Suits Suit { get; set; }
+        public static Ranks Rank { get; set; }
+
+        public Card(Colors color, Suits suit, Ranks rank)
+        {
+            Color = color;
+            Suit = suit;
+            Rank = rank;
+        }
+
+        public enum Colors
+        {
+            Red,
+            Black
+        }
+
+        public enum Suits
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades
+        }
+
+        public enum Ranks
+        {
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King,
+            Ace
+        }
+    }
+}
---
+++ week-06/day-04/TwentyOne/TwentyOne/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Deck
+    {
+        public static List<Card> FillDeck()
+        {
+            var myDeck = new List<Card>();
+
+            for (int i = 0; i < Enum.GetNames(typeof(Card.Colors)).Length; i++)
+            {
+                for (int j = 0; j < Enum.GetNames(typeof(Card.Suits)).Length; j++)
+                {
+                    for (int k = 0; k < Enum.GetNames(typeof(Card.Ranks)).Length; k++)
+                    {
+                        myDeck.Add(new Card(Card.Color = (Card.Colors)i, Card.Suit = (Card.Suits)j, Card.Rank = (Card.Ranks)k));
+                    }
+                }
+            }
+
+            return myDeck;
+        }
+    }
+}
