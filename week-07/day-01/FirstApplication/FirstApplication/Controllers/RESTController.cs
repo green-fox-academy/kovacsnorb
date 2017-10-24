d70b863ec9b8f4daddd1f97252a825d09d112909
kovacsnorb <kovacsnorb@gmail.com>
2017-10-24 14:15:46 +0200

    W7/D1 - Hello World REST part is done

---
+++ week-07/day-01/FirstApplication/FirstApplication/Controllers/RESTController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FirstApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstApplication.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            var myGreeting = new Greeting(1, "Hello, Réka!");
+            return new JsonResult(myGreeting);
+        }
+    }
+}
