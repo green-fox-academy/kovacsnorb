2f11a19b81856d7d206d441d88afb4ea5292b922
kovacsnorb <kovacsnorb@gmail.com>
2017-10-25 13:52:21 +0200

    Bank of Simba - Part I is done

---
+++ week-07/day-02/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BankOfSimba.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BankOfSimba.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("simba")]
+        public IActionResult GetSimba()
+        {
+            var bankAccount = new BankAccount("Simba", "2000", "Animal.Lion");
+            return View(bankAccount);
+        }
+    }
+}
