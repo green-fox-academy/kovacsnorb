cedeec25d08c18378cf97b560a9d82ce0fd37166
kovacsnorb <kovacsnorb@gmail.com>
2017-10-26 14:17:42 +0200

    Counter is done

---
+++ week-07/day-03/Counter/Counter/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CounterApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CounterApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        Counter counter;
+
+        public HomeController(Counter counter)
+        {
+            this.counter = counter;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult AddOne()
+        {
+            counter.PlusOne();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
