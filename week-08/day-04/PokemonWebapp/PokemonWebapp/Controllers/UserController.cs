f51f41284c30418212c250293b7a2ed3f59a1f5a
kovacsnorb <kovacsnorb@gmail.com>
2017-11-02 13:31:32 +0100

    W8/D4 - PokemonWebApp - Login view, User and Pokemon cs, Login and User Controller is created.

---
+++ week-08/day-04/PokemonWebapp/PokemonWebapp/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PokemonWebapp.Controllers
+{
+    [Route("/user")]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult LoginHandler()
+        {
+            return View();
+        }
+    }
+}
---
+++ week-08/day-04/PokemonWebapp/PokemonWebapp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonWebapp.Models
+{
+    public class User
+    {
+        public User()
+        {
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+    }
+}
