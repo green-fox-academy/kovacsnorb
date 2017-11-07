da3f6fff2ada2ab948e0633ecfedc324090ff935
kovacsnorb <kovacsnorb@gmail.com>
2017-11-07 17:00:34 +0100

    W9/D2/RestExercise - /greeter endpoint is done.

--- week-09/day-02/RestExcercise/RestExcercise/Controllers/HomeController.cs
+++ week-09/day-02/RestExcercise/RestExcercise/Controllers/HomeController.cs
@@ -29,5 +29,24 @@ namespace RestExcercise.Controllers
                 return Json(new { received = input, result = input * 2 });
             }
         }
+
+        [HttpGet]
+        [Route("/greeter")]
+        public IActionResult Greeting(string name, string title)
+        {
+            if (name == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+
+            else
+            {
+                return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
+            }
+        }
     }
 }
