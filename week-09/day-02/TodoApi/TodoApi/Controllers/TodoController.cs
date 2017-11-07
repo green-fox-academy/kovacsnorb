a67a0875fc09ccc3f90b1b612a25234d039c280e
kovacsnorb <kovacsnorb@gmail.com>
2017-11-07 14:11:40 +0100

    W9/D2/TodoAPI - Todo Web API Controller is created

---
+++ week-09/day-02/TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Entities;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext todoContext;
+
+        public TodoController(TodoContext todoContext)
+        {
+            this.todoContext = todoContext;
+
+            if (todoContext.TodoItems.Count() == 0)
+            {
+                todoContext.TodoItems.Add(new TodoItem { Name = "Item1" });
+                todoContext.SaveChanges();
+            }
+        }
+    }
+}
