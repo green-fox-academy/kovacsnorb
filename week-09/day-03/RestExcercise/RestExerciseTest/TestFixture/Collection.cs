dfec77b166e542dcfd92bbbbbb99832c50ab3228
kovacsnorb <kovacsnorb@gmail.com>
2017-11-08 16:36:37 +0100

    W9/D3/RestTest - Minor renaming: TextFixfure >> TestFixture

--- week-09/day-03/RestExcercise/RestExerciseTest/TestFixture/Collection.cs
+++ week-09/day-03/RestExcercise/RestExerciseTest/TestFixture/Collection.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
-namespace RestExerciseTest.TextFixture
+namespace RestExerciseTest.TestFixture
 {
     [CollectionDefinition ("BaseCollection")]
     public class Collection : ICollectionFixture<TestContext>
