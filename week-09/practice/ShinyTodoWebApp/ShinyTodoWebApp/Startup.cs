bd011ed1a471143f85f4aca59adb0c10e7c9d1cc
kovacsnorb <kovacsnorb@gmail.com>
2017-11-12 10:11:51 +0100

    Entities map with TodoContext is created. appsettings.json is done. DB configuration is Startup.cs is done.

--- week-09/practice/ShinyTodoWebApp/ShinyTodoWebApp/Startup.cs
+++ week-09/practice/ShinyTodoWebApp/ShinyTodoWebApp/Startup.cs
@@ -6,16 +6,29 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using ShinyTodoWebApp.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShinyTodoWebApp
 {
     public class Startup
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+
             services.AddMvc();
+            services.AddDbContext<TodoContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:TodoConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
