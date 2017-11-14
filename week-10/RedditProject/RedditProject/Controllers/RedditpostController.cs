029764bd37a5baa711e8eac71f70ec3a3f758405
kovacsnorb <kovacsnorb@gmail.com>
2017-11-14 09:40:08 +0100

    Upvote/downvote function is done.

--- week-10/RedditProject/RedditProject/Controllers/RedditpostController.cs
+++ week-10/RedditProject/RedditProject/Controllers/RedditpostController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RedditProject.Repositories;
+using RedditProject.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,6 +25,39 @@ namespace RedditProject.Controllers
         {
             var myPosts = redditpostRepository.FillList();
             return View(myPosts);
+        }
+
+        [Route("posts/add")]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [Route("posts/add")]
+        [HttpPost]
+        public IActionResult Add(Redditpost newPost)
+        {
+            redditpostRepository.AddNew(newPost);
+            return RedirectToAction("ListOfPosts");
+        }
+
+        [Route("posts/{id}/upvote")]
+        [HttpGet]
+        public IActionResult Upvote([FromRoute] long id)
+        {
+            var post = redditpostRepository.GetPostById(id);
+            redditpostRepository.UpVotePost(post);
+            return RedirectToAction("ListOfPosts");
+        }
+
+        [Route("posts/{id}/downvote")]
+        [HttpGet]
+        public IActionResult Downvote([FromRoute] long id)
+        {
+            var post = redditpostRepository.GetPostById(id);
+            redditpostRepository.DownVotePost(post);
+            return RedirectToAction("ListOfPosts");
         }
     }
 }
--- week-10/RedditProject/RedditProject/Repositories/RedditpostRepository.cs
+++ week-10/RedditProject/RedditProject/Repositories/RedditpostRepository.cs
@@ -21,5 +21,30 @@ namespace RedditProject.Repositories
             return redditpostContext.Redditposts.ToList();
         }
 
+        public void AddNew(Redditpost newPost)
+        {
+            redditpostContext.Redditposts.Add(newPost);
+            redditpostContext.SaveChanges();
+        }
+
+        public void UpVotePost(Redditpost postToUpVote)
+        {
+            var post = redditpostContext.Redditposts.FirstOrDefault(p => p.Id == postToUpVote.Id);
+            post.UpVote++;
+            redditpostContext.SaveChanges();
+        }
+
+        public void DownVotePost(Redditpost postToDownVote)
+        {
+            var post = redditpostContext.Redditposts.FirstOrDefault(p => p.Id == postToDownVote.Id);
+            post.DownVote++;
+            redditpostContext.SaveChanges();
+        }
+
+        public Redditpost GetPostById(long id)
+        {
+            return redditpostContext.Redditposts.FirstOrDefault(p => p.Id == id);
+        }
+
     }
 }
