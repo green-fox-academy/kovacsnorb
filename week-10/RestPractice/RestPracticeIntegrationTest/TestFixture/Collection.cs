ebb6b9ea0cb2b471e6bf557e8b1642f40cac4112
kovacsnorb <kovacsnorb@gmail.com>
2017-11-15 12:52:25 +0100

    Backend part of the integration test is done.

---
+++ week-10/RestPractice/RestPracticeIntegrationTest/TestFixture/Collection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RestPracticeIntegrationTest.TestFixture
+{
+    [CollectionDefinition("BaseCollection")]
+    public class Collection : ICollectionFixture<TestContext>
+    {
+    }
+
+}
---
+++ week-10/RestPractice/RestPracticeIntegrationTest/TestFixture/TestContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using RestPractice;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RestPracticeIntegrationTest.TestFixture
+{
+    class TestContext
+    {
+        public HttpClient Client { get; set; }
+        private readonly TestServer Server;
+
+        public TestContext()
+        {
+            Server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            Client = Server.CreateClient();
+        }
+    }
+}
