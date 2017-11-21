7665ad85c00db0114e8cc9586637c2b5e12d6fdc
kovacsnorb <kovacsnorb@gmail.com>
2017-11-21 21:53:24 +0100

    P01-HelloWorld REST app

---
+++ week-11-practice/P01HelloWorld/P01HelloWorld/Controllers/RESTController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using P01HelloWorld.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace P01HelloWorld.Controllers
+{
+    [Route("api")]
+    public class RESTController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        public IActionResult Greeting()
+        {
+            return new JsonResult(new Greeting() { Id = 1, Content = "Hello, World - REST API"});
+        }
+    }
+}
